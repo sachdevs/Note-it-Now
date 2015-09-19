6a1befc37fd23925625a4c55175177e4ba89de4a
Andrew Codispoti <andrewcod749@gmail.com>
2015-09-19 05:14:31 -0400

    More changes.

--- ocr/blobdetection.py
+++ ocr/blobdetection.py
@@ -3,11 +3,11 @@ import numpy as np
 import math
 # Read image
 im = cv2.imread("blob.jpg", cv2.IMREAD_GRAYSCALE)
-def getFilter(minThreshold = 90, maxThreshold = 200, minCircularity = 0, maxCircularity = 1, minConvexity = 0, maxConvexity = 0.95):
+def getFilter(minThreshold = 90, maxThreshold = 200, minCircularity = 0, maxCircularity = 1, minConvexity = 0, maxConvexity = 0.95, minInertiaRatio = 0.5):
     params = cv2.SimpleBlobDetector_Params()
 
-    params.filterByArea = True
-    params.minArea = 800
+    params.filterByArea = False
+    params.minArea = 100
 
     # Change thresholds
     params.minThreshold = minThreshold
@@ -24,24 +24,25 @@ def getFilter(minThreshold = 90, maxThreshold = 200, minCircularity = 0, maxCirc
     params.maxConvexity = maxConvexity
 
     params.filterByInertia = False
+    params.minInertiaRatio = minInertiaRatio
     return params
 
 
 def getTriangleFilter():
-    return getFilter(90, 200, 0.0, 0.6, 0, 0.95)
+    return getFilter(100, 200, 0.4, 0.6, 0.75, 1)
 
 def getSquareFilter():
-    return getFilter(90, 200, 0.6, 0.8, 0, 0.95)
+    return getFilter(100, 200, 0, 0.4, 0, 0.65, 0.3)
 
 def getCircleFilter():
-    return getFilter(90, 200, 0.6, 1.0, 0.5, 0.95)
+    return getFilter(100, 200, 0.9, 1.0, 0.5, 0.95)
 
 
 while True:
-    params = getSquareFilter()
+    params = getTriangleFilter()
     detector = cv2.SimpleBlobDetector(params)
-
     keypoints = detector.detect(im)
+
     print (keypoints)
     im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
     cv2.imshow("Keypoints", im_with_keypoints)
