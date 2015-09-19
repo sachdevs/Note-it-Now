f647940473997a48aa725e81cab234657e7eec98
Andrew Codispoti <andrewcod749@gmail.com>
2015-09-19 11:28:09 -0400

    Adapter to fit margin now.

--- ocr/cropmarkup.py
+++ ocr/cropmarkup.py
@@ -10,8 +10,15 @@ def getMarkupTab(imagePath):
     width,height = original.size
     left = 0
     top = 0
+    rgb  =  original.convert('RGB')
+    for x in range(0,width/3):
+        r,g,b = rgb.getpixel((x,0))
+        if (g < 160):
+            marginSize = x
+    print marginSize
     right = marginSize
     bottom = height
     cropped_example = original.crop((left,top, right, bottom))
     cropped_example.save(imagePath+".sidebar.jpg")
     cropped_example.show()
+# getMarkupTab("landscapebinder.png")
